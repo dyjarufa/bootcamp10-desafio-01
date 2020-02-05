91d435155b288672cf57bb14a66eb6715fcc2a2d
Jady Santos Rufino da Silva <jrufino@anp.gov.br>
2020-02-05 17:18:42 -0300

    commit inicial/rest crud rules

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+server.get('/projects', (req, res) => {
+    return res.json(projects)
+})
+
+server.post('/projects', (req, res) => {
+    const { id, title } = req.body;
+
+    const project = {
+        id,
+        title,
+        tasks: []
+    }
+    projects.push(project);
+
+    return res.json(project);
+})
+
+server.put('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+    const project = projects.find(pro => pro.id == id)
+    project.title = title;
+
+    return res.json(project);
+})
+
+server.delete('/projects/:id', (req, res) => {
+    const { id } = req.params
+    const index = projects.findIndex(p => p.id == id)
+    projects.splice(index, 1);
+
+    return res.json(projects);
+})
+
+server.post('/projects/:id/tasks', (req, res) => {
+    const { id } = req.params
+    const { title } = req.body
+
+    const project = projects.find(p => p.id == id)
+
+    project.tasks.push(title);
+
+    return res.json(project)
+
+})
+
+
+
+server.listen(3000);
